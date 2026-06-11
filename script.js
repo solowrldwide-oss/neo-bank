7051c03d30071f10837af9ee1879dfcefa8190e4
solowrldwide-oss <solowrldwide@gmail.com>
2026-06-11 12:29:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ function login(){
     window.location.href = "dashboard.html";
 }
 
+/* BALANCE TOGGLE */
 function toggleBalance(){
     const balance = document.getElementById("balance");
 
-    if(balance.innerText === "$250,000,000.00"){
+    if(balance.innerText.includes("250")){
         balance.innerText = "**************";
     } else {
         balance.innerText = "$250,000,000.00";
     }
+}
+
+/* SIDEBAR TOGGLE */
+function toggleMenu(){
+    document.getElementById("sidebar").classList.toggle("hide");
+}
+
+/* DARK MODE */
+function toggleDark(){
+    document.body.classList.toggle("dark");
+}
+
+/* NOTIFICATION */
+function notify(){
+    let popup = document.getElementById("popup");
+    popup.style.display = "block";
+
+    setTimeout(()=>{
+        popup.style.display = "none";
+    },2000);
 }
