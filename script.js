4650536075ddee69a0477035b6e68cde2ab291c4
solowrldwide-oss <solowrldwide@gmail.com>
2026-06-11 11:54:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function login(){
     window.location.href = "dashboard.html";
 }
+
+function toggle(){
+    const balance = document.getElementById("balance");
+
+    if(balance.innerText === "$250,000,000.00"){
+        balance.innerText = "************";
+    } else {
+        balance.innerText = "$250,000,000.00";
+    }
+}
